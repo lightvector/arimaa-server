ee58e6e8b0084e12d9d5a51ae9bda199570f5ada
Matthew Jacobs <twinsonpurim@gmail.com>
2015-05-13 23:04:39 -0400

    adding a simple method so I can test it with JUnit.

--- src/Hello.scala
+++ src/Hello.scala
@@ -1,5 +1,16 @@
-object HelloWorld {
+object Hello {
+  def even(n: Int): Boolean = {
+    return n % 2 == 0
+  }
+
+  def odd(n: Int): Boolean = {
+    return n % 2 == 1
+  }
+
   def main(args: Array[String]) {
     println("Hello, world!")
+    println("2 is even: " + even(2))
+    println("3 is even: " + even(3))
+    println("3 is odd: " + odd(3))
   }
 }
