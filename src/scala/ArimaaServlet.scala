d3b040c5a1c96a8ffc878391b48532848700605e
Ubuntu <ubuntu@ip-172-31-11-234.ec2.internal>
2015-05-16 21:40:22 +0000

    add hello world webserver using scalatra, update build file for it, move things around

---
+++ src/scala/ArimaaServlet.scala
@@ -0,0 +1,14 @@
+import org.scalatra._
+import scalate.ScalateSupport
+
+class ArimaaServlet extends WebAppStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+      </body>
+    </html>
+  }
+
+}
