b1c87a90536ac4e2bf01d0292bbb0530e6d19808
lightvector <lightvector@gmail.com>
2015-06-13 01:06:00 -0400

    Add some hello-worldish code for parsing json with scalatra

--- project/Build.scala
+++ project/Build.scala
@@ -6,16 +6,28 @@ object ArimaaServerBuild extends Build {
   lazy val arimaaServer = Project(
     id = "arimaa-server",
     base = file("."),
-  
+
     settings = Seq(
       scalaVersion := "2.11.6",
 
       libraryDependencies ++= Seq(
-        "org.scalatra" %% "scalatra" % "2.4.0.RC1",
-        "org.scalatra" %% "scalatra-scalate" % "2.4.0.RC1",
-        "javax.servlet" % "javax.servlet-api" % "3.1.0",
-        "org.scalatest" %% "scalatest" % "2.2.5" % "test"
-      )
+        "org.scalatra"  %% "scalatra"          % "2.4.0.RC1",
+        "org.scalatra"  %% "scalatra-scalate"  % "2.4.0.RC1",
+        "javax.servlet" %  "javax.servlet-api" % "3.1.0",
+        "org.scalatest" %% "scalatest"         % "2.2.5" % "test",
+        "org.scalatra"  %% "scalatra-json"     % "2.4.0.RC1",
+        "org.json4s"    %% "json4s-jackson"    % "3.3.0.RC1"
+      ),
+
+      //TODO(lightvector): Is this concerning? This is a dependency of scalatra-json, and I guess whoever owns this library
+      //messed up when uploading things and updating the published hash? (either that or I got a bad/hacked version of this library
+    )
+      //Working around error in sbt:
+      //[warn] problem while downloading module descriptor:
+      //https://repo1.maven.org/maven2/com/thoughtworks/paranamer/paranamer/2.7/paranamer-2.7.pom:
+      //invalid sha1: expected=deea673ac6c495cfca3c2fecce26bd9b67295e5b computed=36ef984e4403f800d8aeb5b82045ea4cec0db07b
+      checksums in update := Nil
     ) ++ jetty()
+
   )
 }
--- src/main/scala/org/playarimaa/server/ArimaaServlet.scala
+++ src/main/scala/org/playarimaa/server/ArimaaServlet.scala
@@ -2,14 +2,52 @@ package org.playarimaa.server
 import org.scalatra._
 import scalate.ScalateSupport
 
-class ArimaaServlet extends WebAppStack {
+import org.json4s.{DefaultFormats, Formats}
+import org.scalatra.json._
 
-  get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-      </body>
-    </html>
+case class Response(message: String, timestamp: Long)
+object Response {
+  def create(message: String): Response = {
+    val timestamp = System.currentTimeMillis()
+    Response(message,timestamp)
   }
+}
+
+case class Request(a: Int, b:Int)
+
+//TODO: add error handling for malformed input
+/*
+val errorHandling: PartialFunction[Throwable, Unit] = {
+  case e:java.util.NoSuchElementException => send_error("DB malformed", InternalError)
+  case e => send_error("Internal Error", InternalError)
+}*/
+
+class ArimaaServlet extends WebAppStack with JacksonJsonSupport {
+  // Sets up automatic case class to JSON output serialization, required by
+  // the JValueResult trait.
+  protected implicit lazy val jsonFormats: Formats = DefaultFormats
+
+  // Before every action runs, set the content type to be in JSON format.
+  before() {
+    contentType = formats("json")
+  }
+
+  //curl -i http://localhost:8080/
+  get("/") {
+    Response.create("Hello world")
+  }
+
+  //curl -i -X POST -d "name=sally" http://localhost:8080/test
+  post("/test") {
+    val name = params("name")
+    Response.create("Hello " + name)
+  }
+
+  //curl -i -H "Content-Type: application/json" -X POST -d '{"a":2, "b":3}' http://localhost:8080/test2
+  post("/test2") {
+    val r = parse(request.body).extract[Request]
+    Response.create("" + (r.a + r.b))
+  }
+
 
 }
