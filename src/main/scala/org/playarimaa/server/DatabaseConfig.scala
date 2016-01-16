5f45ade9b18afed48204c08575de8c52f93fbd17
lightvector <lightvector@gmail.com>
2016-01-16 04:00:54 +0000

    Add logging for db choice

--- src/main/scala/org/playarimaa/server/DatabaseConfig.scala
+++ src/main/scala/org/playarimaa/server/DatabaseConfig.scala
@@ -3,10 +3,11 @@ package org.playarimaa.server
 import scala.concurrent.{ExecutionContext, Future, Promise, future}
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
+import slick.driver.{H2Driver, JdbcProfile, PostgresDriver}
+import org.slf4j.{Logger, LoggerFactory}
+
 import org.playarimaa.server.chat._
 import org.playarimaa.server.game._
-
-import slick.driver.{H2Driver, JdbcProfile, PostgresDriver}
 
 sealed trait WhichDB
 case object PostgresDB extends WhichDB
@@ -14,12 +15,18 @@ case object H2DB extends WhichDB
 
 object DatabaseConfig {
 
+  val logger =  LoggerFactory.getLogger(getClass)
+
   //Switch which db we load based on whether we are in prod or test. See /project/Build.scala for the run configurations
   //that take advantage of this
   lazy val whichDB: WhichDB = {
     System.getProperty("isProd") match {
-      case "true" => PostgresDB
-      case "false" => H2DB
+      case "true" =>
+        logger.info("Using PostgresDB")
+        PostgresDB
+      case "false" =>
+        logger.info("Using H2DB")
+        H2DB
       case null => throw new Exception("isProd system property undefined, expected \"true\" or \"false\"")
       case x => throw new Exception("Unexpected value for isProd system property, expected \"true\" or \"false\": " + x)
     }
